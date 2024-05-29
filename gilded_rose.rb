17b70d2fffc91352e1424217f322976f689bbe3a
Viktor Kukhar <kukharviktor@icloud.com>
2024-05-30 00:40:05 +0300

    Using Extract method for main code

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,22 +5,62 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if item.name == "Aged Brie"
-        update_aged_brie(item)
-      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-        update_backstage_passes(item)
-      elsif item.name == "Conjured Mana Cake"
-        update_conjured_item(item)
-      elsif item.name != "Sulfuras, Hand of Ragnaros"
-        update_normal_item(item)
-      end
-
-      decrease_sell_in(item) unless item.name == "Sulfuras, Hand of Ragnaros"
-
-      if item.sell_in < 0
-        handle_expired_item(item)
-      end
+      update_item_quality(item)
+      update_item_sell_in(item)
+      handle_expired_item(item) if item.sell_in < 0
     end
+  end
+
+  private
+
+  def update_item_quality(item)
+    case item.name
+    when "Aged Brie"
+      update_aged_brie(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      update_backstage_passes(item)
+    when "Conjured Mana Cake"
+      update_conjured_item(item)
+    when "Sulfuras, Hand of Ragnaros"
+      # not change
+    else
+      update_normal_item(item)
+    end
+  end
+
+  def update_item_sell_in(item)
+    item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def handle_expired_item(item)
+    case item.name
+    when "Aged Brie"
+      handle_expired_aged_brie(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      handle_expired_backstage_passes(item)
+    when "Conjured Mana Cake"
+      handle_expired_conjured_item(item)
+    when "Sulfuras, Hand of Ragnaros"
+      # not change
+    else
+      handle_expired_normal_item(item)
+    end
+  end
+
+  def handle_expired_aged_brie(item)
+    increase_quality(item)
+  end
+
+  def handle_expired_backstage_passes(item)
+    item.quality = 0
+  end
+
+  def handle_expired_conjured_item(item)
+    2.times { decrease_quality(item) }
+  end
+
+  def handle_expired_normal_item(item)
+    decrease_quality(item)
   end
 
   def update_normal_item(item)
@@ -41,28 +81,12 @@ class GildedRose
     increase_quality(item) if item.sell_in < 6
   end
 
-  def handle_expired_item(item)
-    if item.name == "Aged Brie"
-      increase_quality(item)
-    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-      item.quality = 0
-    elsif item.name == "Conjured Mana Cake"
-      2.times { decrease_quality(item) }
-    elsif item.name != "Sulfuras, Hand of Ragnaros"
-      decrease_quality(item)
-    end
-  end
-
   def increase_quality(item)
     item.quality += 1 if item.quality < 50
   end
 
   def decrease_quality(item)
     item.quality -= 1 if item.quality > 0
-  end
-
-  def decrease_sell_in(item)
-    item.sell_in -= 1
   end
 end
 
